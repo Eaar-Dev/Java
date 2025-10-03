b6afab2d39d2481fea20d916158e2807914bdcf5
Edgar Alejandro Alcocer Reyes <aspire5@outlook.es>
2025-10-03 13:31:35 -0600

    First commit
    - Adding WhileCicles

---
+++ src/Ciclos/While/Example1.java
@@ -0,0 +1,15 @@
+package Ciclos.While;
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("*** Example 01 ***");
+
+        int counter = 1;
+        while (counter <= 20) {
+            if (counter % 2 == 0) {
+                System.out.print(counter + " ");
+            }
+            counter++;
+        }
+    }
+}
---
+++ src/Ciclos/While/WhileCicle.java
@@ -0,0 +1,12 @@
+package Ciclos.While;
+
+public class WhileCicle {
+    public static void main(String[] args) {
+        System.out.println("*** While Cicle ***");
+
+        short counter = 0;
+        while (counter <= 5) {
+            System.out.println(counter++);
+        }
+    }
+}
